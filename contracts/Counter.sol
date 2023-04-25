03a280ca8c8dc7e07f05163bfdfb3834a8aa6009
Sedat Taskiran <se_tas@yahoo.com>
2023-04-25 22:03:07 +0300

    Assignment II completed

---
+++ contracts/Counter.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.7;
+
+contract Counter {
+    uint public total;
+    uint public subtracted;
+    uint public multiplied;
+    uint public divided;
+    
+    function add(uint a, uint b) public {
+        total = a+b;
+    
+    }
+    function subtract(uint a, uint b) public {
+        require(a>=b, "firs num should be greater");
+        subtracted = a - b ;
+    }
+    function multiply(uint a, uint b) public {
+        multiplied = a*b;
+    }
+    function divide(uint a, uint b) public {
+        divided = a/b;
+    }
+
+    function get() public view returns (uint[4] memory){
+        return [total, subtracted, multiplied, divided];
+    }
+}
