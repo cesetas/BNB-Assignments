021e598089da1e32ed6aaa0e18ade19b75f17a21
Sedat Taskiran <se_tas@yahoo.com>
2023-05-02 22:56:30 +0300

    Assignment-IV

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,62 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// const { mnemonic } = require("./secrets.json");
+
+// // This is a sample Hardhat task. To learn how to create your own go to
+// // https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async () => {
+//   const accounts = await ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const BNB_TEST_URL = process.env.BNB_TEST_URL;
+const BNB_PRIVATE_KEY = process.env.BNB_PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "testnet",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    hardhat: {},
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [BNB_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: [],
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 20000,
+  },
 };
